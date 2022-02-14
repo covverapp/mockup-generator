dcab8da224950c05d4b68217cc7e53da726429d6
Itai Reuveni <itai.reuveni@covver.io>
2022-02-14 09:47:50 +0100

    Organize it a bit more

--- playing.js
+++ playing.js
@@ -1,31 +1,69 @@
 var Jimp = require("jimp");
 
+const TRANSPARENT_COLOR = "#00000000";
+
+/**
+ * Pads an image with a transparent border so that weird border effects from
+ * displacing the image will be transparent
+ * @param {Jimp} image to pad
+ * @returns {Jimp} padded image
+ */
 async function createImageWithTransparentPadding(image) {
   return new Jimp(
     image.bitmap.width + 2,
     image.bitmap.height + 2,
-    "#00000000" // Transparent background
+    TRANSPARENT_COLOR
   )
     .composite(image, 1, 1)
     .resize(image.bitmap.width, image.bitmap.height);
 }
 
+/**
+ * Gets the average color of the displacement map. Makes it so the average color
+ * becomes sort of the baseline and everything is displaced forwards and backwards
+ * from said color.
+ * @param {Jimp} image to calculate the average color of
+ * @returns {number} the average color of the greyscale image
+ */
+function getAverageColor(map) {
+  let averageColor = 0;
+  map.scanQuiet(
+    0,
+    0,
+    map.bitmap.width,
+    map.bitmap.height,
+    function (_, _, idx) {
+      averageColor += map.bitmap.data[idx];
+    }
+  );
+  return averageColor / (map.bitmap.width * map.bitmap.height);
+}
+
+/**
+ * Algorithm loosely based from here https://docs.gimp.org/2.6/en/plug-in-displace.html
+ * @param {Jimp} image to apply the displacement on
+ * @param {Jimp} map the greyscale displacement map
+ * @param {number} offset the amount that should be displaced
+ * @returns {Jimp} displaced image
+ */
 async function displace(image, map, offset) {
   const source = await createImageWithTransparentPadding(image);
   const destination = await new Jimp(
     image.bitmap.width,
     image.bitmap.height,
-    "#00000000" // Transparent background
+    TRANSPARENT_COLOR
   );
+  const averageColor = getAverageColor(map);
+
+  console.log(averageColor);
   image.scanQuiet(
     0,
     0,
     image.bitmap.width,
     image.bitmap.height,
     function (x, y, idx) {
-      // Make it so white values (255) are not displaced
-      // This makes it so white is out front and everything else gets displaced back
-      let displacement = ((map.bitmap.data[idx] - 255) / 127.5) * offset;
+      let displacement =
+        ((map.bitmap.data[idx] - averageColor) / 127.5) * offset;
       displacement = Math.round(displacement);
 
       if (
@@ -44,16 +82,22 @@ async function displace(image, map, offset) {
   return destination;
 }
 
-const main = async () => {
-  const x = 400;
-  const y = 617;
-  const blur = 5;
-  const contrast = 0.1;
-  const displacementAmount = 40;
+TANK_PARAMS = [370, 300, 2, 0, 20, "tank.jpg", 260, 350];
+SHIRT_PARAMS = [400, 617, 5, 0.1, 40, "shirt.jpg", 473, 630];
 
-  const mockupImage = await Jimp.read("./static/shirt.jpg");
-  let printfile = await Jimp.read("./static/printfile.png");
-  const displacementMap = mockupImage
+const main = async (params) => {
+  const x = params[0];
+  const y = params[1];
+  const blur = params[2];
+  const contrast = params[3];
+  const displacementAmount = params[4];
+
+  const baseImage = await Jimp.read(`./static/${params[5]}`);
+  let printfile = (await Jimp.read("./static/printfile.png")).resize(
+    params[6],
+    params[7]
+  );
+  const displacementMap = baseImage
     .clone()
     .crop(x, y, printfile.bitmap.width, printfile.bitmap.height);
 
@@ -62,9 +106,9 @@ const main = async () => {
 
   printfile = await displace(printfile, displacementMap, displacementAmount);
   printfile.write("printfile.png");
-  mockupImage.composite(printfile, x, y).write("output.jpg");
+  baseImage.composite(printfile, x, y).scale(0.5).write("output.jpg");
 };
 
-main()
+main(SHIRT_PARAMS)
   .then(() => console.log("DONE"))
   .catch((err) => console.log(err));
